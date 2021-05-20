8ae0b63b80352716e5842d633c437fbab1461c72
artem <eremchuk.it@gmail.com>
2021-05-20 14:01:07 +0400

    added burger-menu, links click

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 const logo = document.querySelector('.logo');
+const cover = document.querySelector('.cover');
 const iconBurgerBtn = document.querySelector('.icon-burger');
-const mobileMenuWrapper = document.querySelector('.mobile-menu__wrapper');
-const mobileMenu = document.querySelector('.mobile-menu');
+const menuLinks = document.querySelectorAll('.nav__list-item-link ');
+const sidebarMenu = document.querySelector('.nav');
 let iconBurgerBtnFlag = false;
 
 iconBurgerBtn.addEventListener('click', () => {
   if(!iconBurgerBtnFlag){
-    mobileMenuWrapper.style.display = 'block';
+    cover.classList.add('open');
     iconBurgerBtn.classList.add('open');
+    sidebarMenu.classList.add('open');
     logo.classList.add('open');
-    mobileMenu.classList.add('open');
     iconBurgerBtnFlag = true;
   } else {
-    mobileMenuWrapper.style.display = 'none';
+    cover.classList.remove('open');
     iconBurgerBtn.classList.remove('open');
+    sidebarMenu.classList.remove('open');
     logo.classList.remove('open');
-    mobileMenu.classList.remove('open');
     iconBurgerBtnFlag = false;
   }
+})
+
+menuLinks.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    menuLinks.forEach(link => link.classList.remove('active'))
+    el.classList.add('active');
+  })
 })
